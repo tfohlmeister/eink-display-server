172210ebb3c828430a1efecfade90b0862500a72
Thorben Fohlmeister <thorben@fohlmeister.com>
2020-07-06 08:31:49 +0200

    Fixed height declaration #1

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import * as express from 'express';
-
 import { ImageService } from './data/image.service';
 import { ConvertService } from './services/convert.service';
 import { LocalImageService } from './services/local.service';
 import { WallhavenImageService } from './services/wallhaven.service';
+
 
 const PORT = Number(process.env.PORT) || 3000;
 const LOCAL_FOLDERS = process.env.LOCAL_FOLDERS ? (process.env.LOCAL_FOLDERS || '').split(',') : ['/images'];
 const LOCAL_EXCLUDE = (process.env.LOCAL_EXCLUDE || '').split(',');
 const LOCAL_SHOW_HIDDEN = Boolean(process.env.LOCAL_SHOW_HIDDEN) || false;
 const EINK_WIDTH = Number(process.env.EINK_WIDTH) || 800;
-const EINK_HEIGHT = Number(process.env.EINK_WIDTH) || 480;
+const EINK_HEIGHT = Number(process.env.EINK_HEIGHT) || 480;
 
 const app = express();
 
