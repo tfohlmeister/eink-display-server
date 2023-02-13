9516135d88183314587f9fa9bfa97c5e21bf1fd3
Thorben Fohlmeister <thorben@fohlmeister.com>
2023-02-13 21:08:37 +0100

    Removed dist folder

--- dist/data/image.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ImageService = void 0;
-class ImageService {
-    constructor(preFetch = true) {
-        this.preFetch = preFetch;
-    }
-}
-exports.ImageService = ImageService;
